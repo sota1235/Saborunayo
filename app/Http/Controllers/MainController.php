86cfde785862769e92e14f03bd3769bc4fdaf5b7
sota1235 <sota1235@gmail.com>
2015-10-04 10:32:04 +0200

    modify: add MainController

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * メインページ周りのルーティングを管理
+ */
+namespace App\Http\Controllers;
+
+/**
+ * メインページ周りのルーティング管理を行う
+ */
+class MainController extends Controller
+{
+  /**
+   * メインページのviewを返す
+   *
+   * @return view
+   */
+  public function index()
+  {
+    return view('index');
+  }
+}
