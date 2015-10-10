94e4245c1eaebd5e08575c17a80057671e48fe7d
sota1235 <sota1235@gmail.com>
2015-10-10 11:05:44 +0200

    fix: fix indent

--- app/Interfaces/Models/UserModelInterface.php
+++ app/Interfaces/Models/UserModelInterface.php
@@ -6,21 +6,21 @@ namespace App\Interfaces\Models;
 
 interface UserModelInterface
 {
-  /**
-   * ユーザを登録する
-   *
-   * @param string $userName
-   *
-   * @return int $result
-   */
-  public function insertUser($userName);
+    /**
+     * ユーザを登録する
+     *
+     * @param string $userName
+     *
+     * @return int $result
+     */
+    public function insertUser($userName);
 
-  /**
-   * ユーザを削除する
-   *
-   * @param string $userName
-   *
-   * @return int $result
-   */
-  public function deleteUser($userName);
+    /**
+     * ユーザを削除する
+     *
+     * @param string $userName
+     *
+     * @return int $result
+     */
+    public function deleteUser($userName);
 }
--- app/Interfaces/Services/YoServiceInterface.php
+++ app/Interfaces/Services/YoServiceInterface.php
@@ -6,28 +6,28 @@ namespace App\Interfaces\Services;
 
 interface YoServiceInterface
 {
-  /**
-   * 登録済みユーザ全員にYoを送る
-   *
-   * @return void
-   */
-  public function sendYo();
+    /**
+     * 登録済みユーザ全員にYoを送る
+     *
+     * @return void
+     */
+    public function sendYo();
 
-  /**
-   * ユーザをYo対象リストに登録する
-   *
-   * @param string $userName
-   *
-   * @return bool $result
-   */
-  public function addUser($userName);
+    /**
+     * ユーザをYo対象リストに登録する
+     *
+     * @param string $userName
+     *
+     * @return bool $result
+     */
+    public function addUser($userName);
 
-  /**
-   * ユーザをYo対象リストから削除する
-   *
-   * @param string $userName
-   *
-   * @return bool $result
-   */
-  public function dropUser($userName);
+    /**
+     * ユーザをYo対象リストから削除する
+     *
+     * @param string $userName
+     *
+     * @return bool $result
+     */
+    public function dropUser($userName);
 }
