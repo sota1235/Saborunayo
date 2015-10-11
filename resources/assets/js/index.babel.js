1c0e578eb8d836c396687ab0ba0dfece710e1234
sota1235 <sota1235@gmail.com>
2015-10-11 10:20:14 +0200

    modify: check GitHub user name when text box changed

--- resources/assets/js/index.babel.js
+++ resources/assets/js/index.babel.js
@@ -7,16 +7,37 @@ var Promise = require('es6-promise').Promise;
 import Ajax from './ajax.babel.js';
 
 $(() => {
-  console.log('test');
+  /* Variables */
+  let $gitName       = $('#github_name');
+  let $yoName        = $('#yo_name');
+  let $gitNameStatus = $('.git_status');
 
-  // test to check git name
   let appUrl = location.protocol + '//' + location.host;
-  let ajax = new Ajax(appUrl + '/check/git');
+  let gitCheckAjax = new Ajax(appUrl + '/check/git');
+  let registerAjax = new Ajax(appUrl + '/register/user');
 
-  ajax.request({ git_name: 'sota1235' })
-    .then(result => console.log(result))
-    .catch(error => console.log(error));
-  ajax.request({ git_name: 'hogeee' })
-    .then(result => console.log(result))
-    .catch(error => console.log(error));
+  /* Functions */
+  // draw GitHub user name status
+  let showStatus = (status) => {
+    $gitNameStatus.text(status);
+  };
+
+  // parse JSON response and judge it is success
+  let checkJson = (json) => {
+    return json.status === 'success';
+  };
+
+  /* Event Listeners */
+  // watch and check GitHub user name
+  $gitName.change(() => {
+    console.log('Debug: text box changed');
+    gitCheckAjax.request({ git_name: $gitName.val() })
+      .then((result) => {
+        return checkJson(result);
+      })
+      .then((result) => {
+        showStatus(result ? 'user name is valid' : 'invalid user name');
+      })
+      .catch(error => console.log(error));
+  });
 });
