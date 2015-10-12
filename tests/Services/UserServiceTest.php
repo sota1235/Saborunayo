c7feb279f370ec379a67037965dc46bb388fa4cf
sota1235 <sota1235@gmail.com>
2015-10-12 18:17:06 +0200

    modify: add test method when registerUser failed

--- tests/Services/UserServiceTest.php
+++ tests/Services/UserServiceTest.php
@@ -21,17 +21,41 @@ class UserServiceTest extends TestCase
     public function testRegisterUserSuccess()
     {
         $this->app->bind('App\Interfaces\Models\UserModelInterface', function () {
-            return new UserMockClass();
+            return new TestRegisterUserSuccess();
         });
         $userService = new UserService($this->app->make('App\Interfaces\Models\UserModelInterface'));
         // assertion
         $this->assertTrue($userService->registerUser('hoge', 'moge'));
     }
+
+    /**
+     * test method for registerUser()
+     * user's registration failed
+     */
+    public function testRegisterUserFailed()
+    {
+        $this->app->bind('App\Interfaces\Models\UserModelInterface', function () {
+            return new TestRegisterUserFailed();
+        });
+        $userService = new UserService($this->app->make('App\Interfaces\Models\UserModelInterface'));
+        // assertion
+        $this->assertFalse($userService->registerUser('hoge', 'moge'));
+    }
 }
 
-class UserMockClass implements \App\Interfaces\Models\UserModelInterface
+abstract class AbstractUserMockClass implements \App\Interfaces\Models\UserModelInterface
 {
-    public function insertUser($gitHubName, $yoName) { return true; }
+    public function insertUser($gitHubName, $yoName) {}
     public function deleteUser($userId) {}
     public function getIdByGitHubName($gitHubName) {}
+}
+
+class TestRegisterUserSuccess extends AbstractUserMockClass
+{
+    public function insertUser($gitHubName, $yoName) { return 1; }
+}
+
+class TestRegisterUserFailed extends AbstractUserMockClass
+{
+    public function insertUser($gitHubName, $yoName) { return 0; }
 }
