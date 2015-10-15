3107fc9139ef29c358da13a80044d15ee7ae56eb
sota1235 <sota1235@gmail.com>
2015-10-15 17:38:37 +0200

    modify: complete test for AjaxController

--- tests/Controllers/AjaxControllerTest.php
+++ tests/Controllers/AjaxControllerTest.php
@@ -46,12 +46,77 @@ class AjaxControllerTest extends TestCase
                  'status' => 'failed',
              ]);
     }
+
+    /**
+     * test method for registerUser
+     * return success status
+     */
+    public function testRegisterUser()
+    {
+        // replace GitHubService with MockClass
+        $this->app->bind('App\Interfaces\Services\UserServiceInterface',
+            'testRegisterUserUser'
+        );
+        $this->app->bind('App\Interfaces\Services\YoServiceInterface',
+            'testRegisterUserYo'
+        );
+        // assertion
+        $this->post('/register/user', ['git_name' => 'sota', 'yo_name' => 'hoge'])
+             ->seeJson([
+                 'status' => 'success',
+             ]);
+    }
+
+    /**
+     * test method for registerUser
+     * return failed status
+     */
+    public function testRegisterUserFailed()
+    {
+        /* case 1: add yo user success */
+        // replace GitHubService with MockClass
+        $this->app->bind('App\Interfaces\Services\UserServiceInterface',
+            'testRegisterUserFailedUser'
+        );
+        $this->app->bind('App\Interfaces\Services\YoServiceInterface',
+            'testRegisterUserYo'
+        );
+        // assertion
+        $this->post('/register/user', ['git_name' => 'sota', 'yo_name' => 'hoge'])
+             ->seeJson([
+                 'status' => 'failed',
+             ]);
+
+        /* case 2: all process failed */
+        // replace GitHubService with MockClass
+        $this->app->bind('App\Interfaces\Services\YoServiceInterface',
+            'testRegisterUserFailedYo'
+        );
+        // assertion
+        $this->post('/register/user', ['git_name' => 'sota', 'yo_name' => 'hoge'])
+             ->seeJson([
+                 'status' => 'failed',
+             ]);
+    }
 }
 
 abstract class GitHubServiceMock implements \App\Interfaces\Services\GitHubServiceInterface
 {
     public function isExist($userName) {}
     public function checkContribution($userName) {}
+}
+
+abstract class UserServiceMock implements \App\Interfaces\Services\UserServiceInterface
+{
+    public function registerUser($gitHubName, $yoName) {}
+    public function dropUser($gitHubName) {}
+}
+
+abstract class YoServiceMock implements \App\Interfaces\Services\YoServiceInterface
+{
+    public function sendYo() {}
+    public function addUser($userName) {}
+    public function dropUser($userName) {}
 }
 
 class testCheckGitHubName extends GitHubServiceMock
@@ -62,4 +127,24 @@ class testCheckGitHubName extends GitHubServiceMock
 class testCheckGitHubNameFailed extends GitHubServiceMock
 {
     public function isExist($userName) { return false; }
+}
+
+class testRegisterUserUser extends UserServiceMock
+{
+    public function registerUser($gitHubName, $yoName) { return true; }
+}
+
+class testRegisterUserYo extends YoServiceMock
+{
+    public function addUser($yoName) { return true; }
+}
+
+class testRegisterUserFailedUser extends UserServiceMock
+{
+    public function registerUser($gitHubName, $yoName) { return false; }
+}
+
+class testRegisterUserFailedYo extends YoServiceMock
+{
+    public function addUser($yoName) { return false; }
 }
