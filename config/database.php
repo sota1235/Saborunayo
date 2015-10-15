e9d2df9110c83ca12f5c61572b8ee269ed3bf1cf
sota1235 <sota1235@gmail.com>
2015-10-15 16:13:48 +0200

    modify: db settings for test

--- config/database.php
+++ config/database.php
@@ -52,6 +52,12 @@ return [
             'prefix'   => '',
         ],
 
+        'test' => [
+            'driver'   => 'sqlite',
+            'database' => database_path('database_test.sqlite'),
+            'prefix'   => '',
+        ],
+
         'mysql' => [
             'driver'    => 'mysql',
             'host'      => env('DB_HOST', 'localhost'),
