6d541b0c3f2bf587d74be0e3345cba8ea31edfc3
sota1235 <sota1235@gmail.com>
2015-10-24 11:18:52 +0200

    modify: add 404 page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.master')
+
+@section('body')
+<div class="container">
+  <div>
+    <h1>Page Not Found</h1>
+    <a href="/" style="font-size: 30px">Return to main page</a>
+  </div>
+</div>
+@stop
