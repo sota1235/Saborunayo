8949ea0fabc4cfc4cfd34bffe72fbd4820ef8c55
sota1235 <sota1235@gmail.com>
2015-10-24 08:51:07 +0200

    add: add web font

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -2,6 +2,7 @@
   <head>
     <title>SaborunaYo</title>
     <meta name="csrf-token" content="{{ csrf_token() }}" />
+    <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="css/app.css">
     @yield('addCss')
     <script src="/js/app.js" type="text/javascript"></script>
