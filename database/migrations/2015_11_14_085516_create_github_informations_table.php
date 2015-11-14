360cfbb133fba02315fde22c0145e82b4bb1a92d
sota1235 <sota1235@gmail.com>
2015-11-14 10:00:39 +0100

    modify: add migration for github information table

---
+++ database/migrations/2015_11_14_085516_create_github_informations_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGithubInformationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('github_infomations', function (BluePrint $table) {
+            $table->increments('github_id')->increments();
+            $table->integer('user_id')->unique();
+            $table->string('token')->unique();
+            $table->integer('id')->unique();
+            $table->string('nickname');
+            $table->string('name');
+            $table->string('email');
+            $table->string('avatar');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('github_infomations');
+    }
+}
