78d46d7c60f47c9be093a17bccf48612315dcbd1
sota1235 <sota1235@gmail.com>
2015-11-14 15:27:44 +0900

    modify: add remember_token column

--- database/migrations/2015_10_04_091802_create_users_table.php
+++ database/migrations/2015_10_04_091802_create_users_table.php
@@ -18,6 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('phone_number')->unique();
             $table->dateTime('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
             $table->integer('deleted_flag')->default(0);
+            $table->rememberToken();
         });
     }
 
