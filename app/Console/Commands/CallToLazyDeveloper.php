a709a2ce70ee82cf9f2be59fc1d89f3258639feb
sota1235 <sota1235@gmail.com>
2015-11-15 04:16:09 +0100

    add: add call command

---
+++ app/Console/Commands/CallToLazyDeveloper.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Aloha\Twilio\TwilioInterface;
+use App\Interfaces\Services\GitHubServiceInterface as GitHubService;
+use App\Interfaces\Services\UserServiceInterface   as UserService;
+
+class CallToLazyDeveloper extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'saborunayo:call';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Call to lazy developers.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @param TwilioInterface  $twilio
+     * @param GitHubService    $gitHubService
+     * @param UserService      $userService
+     * @return mixed
+     */
+    public function handle(
+        TwilioInterface $twilio,
+        GitHubService   $gitHubService,
+        UserService     $userService
+    ) {
+        $users = $userService->getUsers();
+        foreach ($users as $user) {
+            if (
+                !$gitHubService->checkContribution($user->name) &&
+                $user->phone_number !== 'tmp'
+            ) {
+                $twilio->call($user->phone_number, function ($message) {
+                    $message->say('Shinchoku Do desuka');
+                    sleep(1);
+                });
+            }
+        }
+    }
+}
